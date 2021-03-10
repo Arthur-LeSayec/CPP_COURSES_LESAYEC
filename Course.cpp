274dc950a3a1d8824b13524819f9e985e305c987
Arthur Le Sayec <arthur.lesayec@gmail.com>
2021-03-10 16:55:43 +0100

    Create Course.cpp

---
+++ Course.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include "Course.h"
+
+using namespace std;
+
+void Course::afficherMeilleurScore (int m_score)
+{ 
+
+
+}
+
+float Course:: moyenne ()
+{
+    
+}
