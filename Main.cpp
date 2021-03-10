c8c00d70da195d4676a87561408394e3692e860a
Arthur Le Sayec <arthur.lesayec@gmail.com>
2021-03-10 16:55:28 +0100

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Course.h"
+#include "Course.cpp"
+
+using namespace std;
+
+int main()
+{
+    Course course1= Course ("Arc-en-ciel");
+    Course course2= Course ("Champs");
+    Course course3= Course ("Chez dédé");
+    Course course4= Course ("La Poste");
+    Course course5= Course ("Mont Fuji");
+
+    
+}
